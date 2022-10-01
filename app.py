25240e3effd6a44a4d0274d79006c38fb9a6ff3b
Winnie Lin <winnie771737@gmail.com>
2022-10-01 14:47:38 +0800

    回傳貼圖

--- app.py
+++ app.py
@@ -43,7 +43,10 @@ def handle_message(event):
     meg = event.message.text
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=s))
+        StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+        ))
 
 
 if __name__ == "__main__":
